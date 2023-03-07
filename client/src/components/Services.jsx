1ce1d6c993e7a2cc333bde1ac714fc69108e052e
erichudson <defideveric@gmail.com>
2023-03-07 08:56:39 -0500

    Transactions.jsx

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -8,12 +8,16 @@ const ServiceCard = ({color, title, icon, subtitle}) => (
     <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
       {icon}
     </div>
+    <div className='ml-5 flex flex-col flex-1'>
+      <h1 className='mt-2 text-white text-lg'>{title}</h1>
+      <p className='mt-2 text-white text-sm md: w-9/12'>{subtitle}</p>
+    </div>
   </div>
 )
 
 const Services = () => {
   return (
-    <div className='flex w-full justify-center items-center gradient-bg-services'>
+    <div className='flex flex-col md:flex-row w-full justify-center items-center gradient-bg-services'>
       <div className='flex mf:flex-row flex-col items-center justify-between md: p-20 py-12 px-4'>
         <div className='flex-1 flex flex-col justify-start items-start'>
           <h1 className='text-white text-3xl sm:text-5xl py-2 text-gradient'>
@@ -24,8 +28,20 @@ const Services = () => {
       <div className='flex-1 flex flex-col justify-start items-center'>
         <ServiceCard
           color='bg-[#2952e3]'
-          title="Securit Guarantee"
+          title="Security Guarantee"
           icon={<BsShieldFillCheck fontSize={21} className='text-white'/>}
+          subtitle='Security is guaranteed. We always maintain your privacy and quality of our products.'
+        />
+         <ServiceCard
+          color='bg-[#8945f8]'
+          title="Best Exchange Rates"
+          icon={<BiSearchAlt fontSize={21} className='text-white'/>}
+          subtitle='Security is guaranteed. We always maintain your privacy and quality of our products.'
+        />
+         <ServiceCard
+          color='bg-[#f84550]'
+          title="Fastes Transactions"
+          icon={<RiHeart2Fill fontSize={21} className='text-white'/>}
           subtitle='Security is guaranteed. We always maintain your privacy and quality of our products.'
         />
       </div>
